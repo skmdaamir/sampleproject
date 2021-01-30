9bb17f63aaa691db7300373175cf96797837495e
skmdaamir <skmdaamir29@gmail.com>
2021-01-30 21:26:19 +0530

    completed Dropdownlist

--- lib/main.dart
+++ lib/main.dart
@@ -14,39 +14,61 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+enum Animals { Cat, Dog, Bird, Lizard, Fish }
+
 class _MyAppState extends State<MyApp> {
-  double _value = 0.0;
-  void onChange(double value) => setState(() => _value = value);
+  Animals _selected = Animals.Cat;
+  String _value = "Make a Selection";
+  List<PopupMenuEntry<Animals>> _items = new List<PopupMenuEntry<Animals>>();
+
+  @override
+  void initState() {
+    for (Animals animal in Animals.values) {
+      _items.add(new PopupMenuItem(
+        child: Text(
+          _getDisplay(animal),
+        ),
+        value: animal,
+      ));
+    }
+  }
+
+  void _onSelected(Animals animal) {
+    setState(() {
+      _selected = animal;
+      _value = "You Selected ${_getDisplay(animal)}";
+    });
+  }
+
+  String _getDisplay(Animals animal) {
+    int index = animal.toString().indexOf('.');
+    index++;
+    return animal.toString().substring(index);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: new Text("Name Here"),
-      ),
-      body: Container(
-        padding: new EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
+        appBar: AppBar(
+          title: new Text("Name Here"),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(32.0),
+          child: Row(
             children: [
-              new Slider(value: _value, onChanged: onChange),
               new Container(
-                padding: EdgeInsets.all(32.0),
-                child: new LinearProgressIndicator(
-                  value: _value,
-                  valueColor: new AlwaysStoppedAnimation<Color>(Colors.green),
-                ),
+                padding: EdgeInsets.all(5.0),
+                child: Text(_value),
               ),
-              new Container(
-                padding: EdgeInsets.all(32.0),
-                child: new CircularProgressIndicator(
-                  value: _value,
-                ),
-              )
+              new PopupMenuButton<Animals>(
+                  child: new Icon(Icons.input),
+                  initialValue: Animals.Cat,
+                  onSelected: _onSelected,
+                  itemBuilder: (BuildContext context) {
+                    return _items;
+                  })
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
